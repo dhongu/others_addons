d19912cfc46c62e753a8885e7e6a1fbca6054ce9
Dorin Hongu <dhongu@gmail.com>
2019-04-09 09:43:37 +0300

    Nu trebuie sa permita depasirea, chiar daca are grupul partner_credit_limit.group_credit_limit

--- partner_credit_limit/models/sale.py
+++ partner_credit_limit/models/sale.py
@@ -13,34 +13,32 @@ class SaleOrder(models.Model):
 
     @api.multi
     def check_limit(self):
-        if not self.env.user.has_group('partner_credit_limit.group_credit_limit'):
-            self.ensure_one()
-            partner = self.partner_id
-            moveline_obj = self.env['account.move.line']
+        self.ensure_one()
+        partner = self.partner_id
+        moveline_obj = self.env['account.move.line']
 
-            movelines = moveline_obj.search([
-                ('partner_id', '=', partner.id),
-                ('full_reconcile_id', '=', False),
-                ('account_id.internal_type', 'in', ['receivable', 'payable'])
-            ])
-            debit, credit = 0.0, 0.0
-            today_dt = fields.Date.today()
-            for line in movelines:
-                date_maturity = line.date_maturity + datetime.timedelta(days=partner.clemency_days)
-                if date_maturity < fields.Date.today(): # and line.user_type_id.type in ['receivable', 'payable']:
-                    credit += line.debit
-                    debit += line.credit
+        movelines = moveline_obj.search([
+            ('partner_id', '=', partner.id),
+            ('full_reconcile_id', '=', False),
+            ('account_id.internal_type', 'in', ['receivable', 'payable'])
+        ])
+        debit, credit = 0.0, 0.0
+        today_dt = fields.Date.today()
+        for line in movelines:
+            date_maturity = line.date_maturity + datetime.timedelta(days=partner.clemency_days)
+            if date_maturity < fields.Date.today(): # and line.user_type_id.type in ['receivable', 'payable']:
+                credit += line.debit
+                debit += line.credit
 
-            if partner.credit_limit and ((credit - debit + self.amount_total) > partner.credit_limit):
-                if not partner.over_credit:
-                    msg = 'Can not confirm Sale Order,Total mature due Amount ' \
-                          '%s as on %s !\nCheck Partner Accounts or Credit ' \
-                          'Limits !' % (credit - debit, today_dt)
-                    raise UserError(_('Credit Over Limits !\n' + msg))
-                partner.write({'credit_limit': credit - debit + self.amount_total})
-            return True
-        else:
-            return True
+        if partner.credit_limit and ((credit - debit + self.amount_total) > partner.credit_limit):
+            if not partner.over_credit:
+                msg = 'Can not confirm Sale Order,Total mature due Amount ' \
+                      '%s as on %s !\nCheck Partner Accounts or Credit ' \
+                      'Limits !' % (credit - debit, today_dt)
+                raise UserError(_('Credit Over Limits !\n' + msg))
+            partner.write({'credit_limit': credit - debit + self.amount_total})
+        return True
+
 
     @api.multi
     def action_confirm(self):
