74f94f6cd29041e3bba08566d04acc32e9f1d66f
Dorin Hongu <dhongu@gmail.com>
2019-07-31 07:34:08 +0300

    adaugare discount global la valoarea fara tva adaugare discount max in configurare

--- sale_global_discounts/models/res_config.py
+++ sale_global_discounts/models/res_config.py
@@ -4,22 +4,26 @@ from odoo import fields, models, api, _
 import werkzeug.urls
 from odoo.exceptions import AccessError
 
+
 class ResCompany(models.Model):
     _inherit = "res.company"
 
     sale_discount_product_id = fields.Many2one('product.product', string='Sale Discount Product')
+    discount_percentage_max = fields.Float("Discount Percentage Max")
 
 
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
     sale_discount_product_id = fields.Many2one('product.product', string='Sale Discount Product')
+    discount_percentage_max = fields.Float("Discount Percentage Max")
 
     @api.model
     def get_values(self):
         res = super(ResConfigSettings, self).get_values()
         res.update(
             sale_discount_product_id=self.env.user.company_id.sale_discount_product_id and self.env.user.company_id.sale_discount_product_id.id or False,
+            discount_percentage_max=self.env.user.company_id.discount_percentage_max
         )
         return res
 
@@ -29,5 +33,7 @@ class ResConfigSettings(models.TransientModel):
         if not self.env.user._is_admin():
             raise AccessError(_("Only administrators can change the settings"))
 
-        self.env.user.company_id.sale_discount_product_id = self.sale_discount_product_id.id
-
+        self.env.user.company_id.write({
+            'sale_discount_product_id': self.sale_discount_product_id.id,
+            'discount_percentage_max': self.discount_percentage_max,
+        })
--- sale_global_discounts/models/sale.py
+++ sale_global_discounts/models/sale.py
@@ -3,19 +3,35 @@
 from odoo import fields, models, api, _
 from odoo.exceptions import UserError
 
+
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    global_discount = fields.Boolean("Add Global Disocunt", readonly=True, states={'draft': [('readonly', False)]},)
-    discount_type = fields.Selection([('fixed','Fixed'),('percentage','Percentage')], 
-        "Disocunt Type", readonly=True, states={'draft': [('readonly', False)]}, default='fixed')
-    discount_amount = fields.Float("Disocunt Amount", readonly=True, states={'draft': [('readonly', False)]},)
-    discount_percentage = fields.Float("Disocunt Percentage", readonly=True, states={'draft': [('readonly', False)]},)
+    global_discount = fields.Boolean("Add Global Discount", readonly=True, states={'draft': [('readonly', False)]}, )
+    discount_type = fields.Selection([('fixed', 'Fixed'), ('percentage', 'Percentage')],
+                                     "Discount Type", readonly=True, states={'draft': [('readonly', False)]},
+                                     default='percentage')
+    discount_amount = fields.Float("Discount Amount", readonly=True, states={'draft': [('readonly', False)]}, )
+    discount_percentage = fields.Float("Discount Percentage", readonly=True, states={'draft': [('readonly', False)]}, )
 
     @api.multi
     def _discount_unset(self):
         if self.env.user.company_id.sale_discount_product_id:
-            self.env['sale.order.line'].search([('order_id', 'in', self.ids), ('product_id', '=', self.env.user.company_id.sale_discount_product_id.id)]).unlink()
+            self.env['sale.order.line'].search([('order_id', 'in', self.ids), (
+            'product_id', '=', self.env.user.company_id.sale_discount_product_id.id)]).unlink()
+
+    @api.onchange('discount_percentage','discount_amount')
+    def onchange_discount_percentage(self):
+        if self.discount_type == 'percentage':
+            if self.discount_percentage and self.env.user.company_id.discount_percentage_max:
+                if self.discount_percentage > self.env.user.company_id.discount_percentage_max:
+                    self.discount_percentage = self.env.user.company_id.discount_percentage_max
+        else:
+            if self.discount_amount and self.env.user.company_id.discount_percentage_max:
+                amount = (self.amount_untaxed * self.env.user.company_id.discount_percentage_max) / 100
+                if self.discount_amount > amount:
+                    self.discount_amount = amount
+
 
     @api.multi
     def create_discount(self):
@@ -33,7 +49,7 @@ class SaleOrder(models.Model):
             if order.discount_type == 'fixed':
                 amount = order.discount_amount
             if order.discount_type == 'percentage':
-                amount = (order.amount_total * order.discount_percentage)/100
+                amount = (order.amount_untaxed * order.discount_percentage) / 100
 
             # Create the Sale line
             Line.create({
