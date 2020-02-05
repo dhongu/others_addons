4b716ea9e8baa2e31b380dcfaaa528e8e36d7aab
danila12 <danila@terrabit.ro>
2020-02-05 13:30:32 +0200

    added muk_web_theme

--- muk_web_theme/models/res_config_settings.py
+++ muk_web_theme/models/res_config_settings.py
@@ -1,19 +1,22 @@
 ###################################################################################
 #
-#    Copyright (C) 2017 MuK IT GmbH
+#    Copyright (c) 2017-2019 MuK IT GmbH.
+#
+#    This file is part of MuK Backend Theme 
+#    (see https://mukit.at).
 #
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
+#    it under the terms of the GNU Lesser General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
+#    GNU Lesser General Public License for more details.
 #
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    You should have received a copy of the GNU Lesser General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 ###################################################################################
 
@@ -34,10 +37,25 @@ class ResConfigSettings(models.TransientModel):
     # Database
     #----------------------------------------------------------
     
+    module_muk_web_theme_mail = fields.Boolean(
+        string="Theme Mail",
+        help="Optimizes the mail chatter for the theme.")
+    
+    module_muk_web_theme_branding = fields.Boolean(
+        string="Theme Branding",
+        help="Customize the theme according to your needs.")
+    
+    module_muk_web_theme_website = fields.Boolean(
+        string="Theme Website",
+        help="Add theme styled website navigation.")
+    
+    module_muk_web_theme_mobile = fields.Boolean(
+        string="Theme Mobile",
+        help="Allow Odoo to be used as a PWA app.")
+    
     theme_background_image = fields.Binary(
         related="company_id.background_image",
-        readonly=False,
-        required=True)
+        readonly=False)
     
     theme_background_blend_mode = fields.Selection(
         related="company_id.background_blend_mode",
@@ -56,6 +74,9 @@ class ResConfigSettings(models.TransientModel):
     
     theme_color_primary = fields.Char(
         string="Theme Primary Color")
+    
+    theme_color_required = fields.Char(
+        string="Theme Required Color")
     
     theme_color_menu = fields.Char(
         string="Theme Menu Color")
@@ -77,6 +98,7 @@ class ResConfigSettings(models.TransientModel):
         variables = [
             'o-brand-odoo',
             'o-brand-primary',
+            'mk-required-color',
             'mk-apps-color',
             'mk-appbar-color',
             'mk-appbar-background',
@@ -87,6 +109,7 @@ class ResConfigSettings(models.TransientModel):
         colors_changed = []
         colors_changed.append(self.theme_color_brand != colors['o-brand-odoo'])
         colors_changed.append(self.theme_color_primary != colors['o-brand-primary'])
+        colors_changed.append(self.theme_color_required != colors['mk-required-color'])
         colors_changed.append(self.theme_color_menu != colors['mk-apps-color'])
         colors_changed.append(self.theme_color_appbar_color != colors['mk-appbar-color'])
         colors_changed.append(self.theme_color_appbar_background != colors['mk-appbar-background'])
@@ -94,6 +117,7 @@ class ResConfigSettings(models.TransientModel):
             variables = [
                 {'name': 'o-brand-odoo', 'value': self.theme_color_brand or "#243742"},
                 {'name': 'o-brand-primary', 'value': self.theme_color_primary or "#5D8DA8"},
+                {'name': 'mk-required-color', 'value': self.theme_color_required or "#d1dfe6"},
                 {'name': 'mk-apps-color', 'value': self.theme_color_menu or "#f8f9fa"},
                 {'name': 'mk-appbar-color', 'value': self.theme_color_appbar_color or "#dee2e6"},
                 {'name': 'mk-appbar-background', 'value': self.theme_color_appbar_background or "#000000"},
@@ -111,6 +135,7 @@ class ResConfigSettings(models.TransientModel):
         variables = [
             'o-brand-odoo',
             'o-brand-primary',
+            'mk-required-color',
             'mk-apps-color',
             'mk-appbar-color',
             'mk-appbar-background',
@@ -121,6 +146,7 @@ class ResConfigSettings(models.TransientModel):
         res.update({
             'theme_color_brand': colors['o-brand-odoo'],
             'theme_color_primary': colors['o-brand-primary'],
+            'theme_color_required': colors['mk-required-color'],
             'theme_color_menu': colors['mk-apps-color'],
             'theme_color_appbar_color': colors['mk-appbar-color'],
             'theme_color_appbar_background': colors['mk-appbar-background'],
