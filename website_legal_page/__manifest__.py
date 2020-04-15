314ef27f76c3f4a39d6f033693cd2cca3dcf3a00
Dorin Hongu <dhongu@gmail.com>
2020-04-15 11:40:43 +0300

    adaugare modul cookie

---
+++ website_legal_page/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2015 Antonio Espinosa <antonioea@antiun.com>
+# Copyright 2017 Jairo Llopis <jairo.llopis@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': "Website Legal Page",
+    'category': 'Website',
+    'version': '12.0.1.1.0',
+    'depends': [
+        'website',
+    ],
+    'data': [
+        'views/reusable_templates.xml',
+        'views/website_legal.xml',
+        'views/website_privacy.xml',
+        'views/website_terms.xml',
+    ],
+    'author': 'Tecnativa, '
+              'LasLabs, '
+              'Nicolas JEUDY, '
+              'Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/website',
+    'license': 'AGPL-3',
+}
