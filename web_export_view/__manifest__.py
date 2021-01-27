162ca8a8e35a931dab3073770863f2e22f349797
Dorin Hongu <dhongu@gmail.com>
2021-01-27 08:12:26 +0200

    adaugare dummy web_export_view

---
+++ web_export_view/__manifest__.py
@@ -0,0 +1,18 @@
+# ©  2008-2019 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Deltatech Dummy Web Export View",
+    "version": "12.0.1.0.0",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "summary": "Generic module",
+
+    "category": "Administration",
+    "depends": ["web", "base"],
+
+    "license": "LGPL-3",
+    "development_status": "stable",
+    "maintainers": ["dhongu"],
+}
