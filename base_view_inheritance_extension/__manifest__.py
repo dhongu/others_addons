aaf06aa85d369db29c4dc395e0bc94bfa2417b9f
Dorin Hongu <dhongu@gmail.com>
2021-02-03 17:33:23 +0200

    Adaugare modul Extended view inheritance

---
+++ base_view_inheritance_extension/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2016 Therp BV <https://therp.nl>
+# Copyright 2018 Tecnativa - Sergio Teruel
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+{
+    "name": "Extended view inheritance",
+    "version": "13.0.1.0.2",
+    "author": "Therp BV,Odoo Community Association (OCA)",
+    "license": "LGPL-3",
+    "category": "Hidden/Dependency",
+    "summary": "Adds more operators for view inheritance",
+    "depends": ["base"],
+    "external_dependencies": {"python": ["pyyaml"]},
+    "demo": ["demo/ir_ui_view.xml"],
+}
