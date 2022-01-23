fc291d5028a204764666f7096d81ff16ed548351
Dorin Hongu <dhongu@gmail.com>
2022-01-23 11:37:34 +0200

    eliminare module

--- account_reconciliation_widget/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import account_move
-from . import account_bank_statement
-from . import account_journal
-from . import reconciliation_widget
-from . import res_company
--- account_reconciliation_widget/readme/CONTRIBUTORS.rst
+++
@@ -1,1 +0,0 @@
-* Tecnativa - Pedro M. Baeza
