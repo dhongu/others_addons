a977d02520e219139b1c07ff9f49bb94d31325cb
Dorin Hongu <dhongu@gmail.com>
2022-11-22 21:01:11 +0200

    Job Queue update

--- queue_job/__manifest__.py
+++ queue_job/__manifest__.py
@@ -1,9 +1,8 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
 
-
 {
     "name": "Job Queue",
-    "version": "14.0.1.3.0",
+    "version": "15.0.2.0.1",
     "author": "Camptocamp,ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/queue",
     "license": "LGPL-3",
@@ -17,11 +16,20 @@
         "views/queue_job_channel_views.xml",
         "views/queue_job_function_views.xml",
         "wizards/queue_jobs_to_done_views.xml",
+        "wizards/queue_jobs_to_cancelled_views.xml",
         "wizards/queue_requeue_job_views.xml",
         "views/queue_job_menus.xml",
         "data/queue_data.xml",
         "data/queue_job_function_data.xml",
     ],
+    "assets": {
+        "web.assets_backend": [
+            "/queue_job/static/lib/vis/vis-network.min.css",
+            "/queue_job/static/src/scss/queue_job_fields.scss",
+            "/queue_job/static/lib/vis/vis-network.min.js",
+            "/queue_job/static/src/js/queue_job_fields.js",
+        ],
+    },
     "installable": True,
     "development_status": "Mature",
     "maintainers": ["guewen"],
--- queue_job/fields.py
+++ queue_job/fields.py
@@ -40,7 +40,7 @@ class JobSerialized(fields.Field):
     def __init__(self, string=fields.Default, base_type=fields.Default, **kwargs):
         super().__init__(string=string, _base_type=base_type, **kwargs)
 
-    def _setup_attrs(self, model, name):
+    def _setup_attrs(self, model, name):  # pylint: disable=missing-return
         super()._setup_attrs(model, name)
         if self._base_type not in self._default_json_mapping:
             raise ValueError("%s is not a supported base type" % (self._base_type))
@@ -107,7 +107,8 @@ class JobDecoder(json.JSONDecoder):
         type_ = obj["_type"]
         if type_ == "odoo_recordset":
             model = self.env(user=obj.get("uid"), su=obj.get("su"))[obj["model"]]
-
+            if obj.get("context"):
+                model = model.with_context(**obj.get("context"))
             return model.browse(obj["ids"])
         elif type_ == "datetime_isoformat":
             return dateutil.parser.parse(obj["value"])
