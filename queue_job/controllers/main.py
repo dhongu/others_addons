9f22dffb14ce2dc0f5c388fd6aada7bd4a55b28f
Dorin Hongu <dhongu@gmail.com>
2024-04-24 10:11:09 +0300

    update

--- queue_job/controllers/main.py
+++ queue_job/controllers/main.py
@@ -34,6 +34,9 @@ class RunJobController(http.Controller):
         _logger.debug("%s started", job)
 
         job.perform()
+        # Triggers any stored computed fields before calling 'set_done'
+        # so that will be part of the 'exec_time'
+        env.flush_all()
         job.set_done()
         job.store()
         env.flush_all()
@@ -175,6 +178,16 @@ class RunJobController(http.Controller):
         size=1,
         failure_rate=0,
     ):
+        """Create test jobs
+
+        Examples of urls:
+
+        * http://127.0.0.1:8069/queue_job/create_test_job: single job
+        * http://127.0.0.1:8069/queue_job/create_test_job?size=10: a graph of 10 jobs
+        * http://127.0.0.1:8069/queue_job/create_test_job?size=10&failure_rate=0.5:
+          a graph of 10 jobs, half will fail
+
+        """
         if not http.request.env.user.has_group("base.group_erp_manager"):
             raise Forbidden(_("Access Denied"))
 
