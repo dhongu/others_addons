507f47f0244787f249d00d584f9a04b1c19f4925
Dorin Hongu <dhongu@gmail.com>
2024-12-02 19:49:02 +0200

    update

--- website_sale_hide_price/__manifest__.py
+++ website_sale_hide_price/__manifest__.py
@@ -10,11 +10,11 @@
     "summary": "Hide product prices on the shop",
     "depends": ["website_sale"],
     "data": [
-        # "data/product_snippet_template_data.xml",
-        # "views/partner_view.xml",
-        # "views/product_template_views.xml",
-        # "views/res_config_settings_views.xml",
-        # "views/website_sale_template.xml",
+        "data/product_snippet_template_data.xml",
+        "views/partner_view.xml",
+        "views/product_template_views.xml",
+        "views/res_config_settings_views.xml",
+        "views/website_sale_template.xml",
     ],
     "installable": True,
     "assets": {
